88e3e92ccc4424cb2fa530be228c98187fc9b075
Orhan Aygün <orhan.aygun@outlook.com>
2017-11-29 15:07:25 +0300

    Sınav projesi eklendi

---
+++ 20171129SinavUygulamasi/kullanicilar.js
@@ -0,0 +1,28 @@
+﻿[
+    {
+        "kullanici_adi":"te.ori",
+        "sifre": "123456",
+        "isim": "Orhan",
+        
+        "soyisim": "Aygün",
+        "profil_resmi": "/content/orhan.jpg",
+        "d_tarihi": "01.01.1985",
+        "mezun_oldgu_okul": "Gazi Üni",
+        "fakulte": "ESEF",
+        "ozgecmis": "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit",
+        "konular":["c#","javascript","html"]
+    },
+    {
+        "kullanici_adi": "mah",
+        "sifre": "123456",
+        "isim": "Mahmut",
+
+        "soyisim": "Aygün",
+        "profil_resmi": "/content/mahmut.jpg",
+        "d_tarihi": "01.01.1987",
+        "mezun_oldgu_okul": "Ankara Üni",
+        "fakulte": "Eğitim Fakültesi",
+        "ozgecmis": "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit",
+        "konular": ["c#", "javascript", "html"]
+    }
+]
